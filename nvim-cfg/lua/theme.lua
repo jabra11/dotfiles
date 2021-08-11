6a4c05c1b296eedfe07bfed05568281fa8ed7ea3
jabra98 <jabra98.gpg@gmail.com>
2021-08-11 03:58:24 +0200

    adapt to upstream syntax change

--- nvim-cfg/lua/theme.lua
+++ nvim-cfg/lua/theme.lua
@@ -4,7 +4,7 @@ vim.g.theme = theme
 local present, base16 = pcall(require, "base16")
 
 if present then
-    base16(base16.themes[theme], true)
+    base16(base16.themes(theme), true)
     require "highlights"
     return true
 else
