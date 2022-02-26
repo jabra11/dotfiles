218ae5b9c8d513b49e0a3814601551f141846161
jabra98 <jabra98.gpg@gmail.com>
2022-02-26 15:03:04 +0100

    nvim tree changes

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,77 +1,88 @@
---local present, nvimtree = pcall(require, "nvim-tree")
---
---if not present then
---   return
---end
+-- init.lua
 
 local g = vim.g
-
-vim.o.termguicolors = true
-
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
---g.nvim_tree_git_hl = 0
-g.nvim_tree_refresh_wait = 100
---g.nvim_tree_gitignore = 0
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-g.nvim_tree_special_files = {}
---
-g.nvim_tree_show_icons = {
-   folders = 1,
-   -- folder_arrows= 1
-   files = 1,
-   git = 0,
-}
-
-g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-      -- arrow_open = "",
-      -- arrow_closed = "",
-      default = "",
-      empty = "", -- 
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
-}
-
-local present, nvimtree = pcall(require, "nvim-tree")
-
-if not present then
-   return
-end
-
-nvimtree.setup {
-   hide_dotfiles = true,
-   disable_netrw = true,
-   hijack_netrw = true,
-   auto_close = false,
-   open_on_tab = true,
-   hijack_cursor = true,
-   update_cwd = false,
-   update_focused_file = {
-      enable = false,
-      update_cwd = false,
-   },
-   view = {
-      allow_resize = true,
-      side = "left",
-      width = 40,
-   },
+g.nvim_tree_special_files = {} 
+-- following options are the default
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+require("nvim-tree").setup {
+  disable_netrw        = false,
+  hijack_netrw         = true,
+  open_on_setup        = false,
+  ignore_ft_on_setup   = {},
+  auto_close           = false,
+  auto_reload_on_write = true,
+  open_on_tab          = false,
+  hijack_cursor        = false,
+  update_cwd           = false,
+  hijack_unnamed_buffer_when_opening = false,
+  hijack_directories   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  git = {
+    enable = false,
+    ignore = false,
+    timeout = 500,
+  },
+  view = {
+    width = 40,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    auto_resize = false,
+    preserve_window_proportions = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    },
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes"
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+      global = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = {
+            "notify",
+            "packer",
+            "qf"
+          }
+        }
+      }
+    }
+  }
 }
